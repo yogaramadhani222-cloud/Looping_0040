41fd8a2f9c29e281df901cfaf44ec224afd8adc1
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-30 16:36:26 +0700

    menambahkan output hasil akhir dari perulangan

---
+++ commit15.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int x;
+
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i <= 4; i++) {
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+
+    cout << endl;
+
+    srand(time(0));
+
+    cout << "PERULANGAN WHILE" << endl;
+    x = 1 + rand() % 10;
+
+    while (x <= 5) {
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    }
+
+    cout << "Bilangan terakhir = " << x << endl;
+
+    cout << "PERULANGAN DO WHILE" << endl;
+    x = 1 + rand() % 10;
+
+    do {
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    } while (x <= 5);
+}
