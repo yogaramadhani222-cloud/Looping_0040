923f52bb496b27ef3f168b9b3deab4f5fbe00906
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-30 16:27:39 +0700

    membuat fungsi input untuk menampilkan menu jurusan

---
+++ commit4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int kodejurusan;
+string namajurusan;
+string statusjurusan;
+
+void input()
+{
+    cout << "=====PENDAFTARAN JURUSAN KAMPUS=====" << endl;
+    cout << "1.Teknologi Informasi" << endl;
+    cout << "2.Teknik Elektro" << endl;
+    cout << "3.Sistem Informasi" << endl;
+}
+
+int main()
+{
+    return 0;
+}
