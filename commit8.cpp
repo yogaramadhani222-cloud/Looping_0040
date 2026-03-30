94ba452059a47ce62a719009619f8ba747965ff0
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-30 16:30:59 +0700

    membuat fungsi output dan menampilkan hasil seleksi

---
+++ commit8.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int kodeJurusan;
+string namaJurusan;
+string statusKuota;
+
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    cin >> kodeJurusan;
+}
+
+void pilihanJurusan() {
+    switch (kodeJurusan) {
+        case 1:
+            namaJurusan = "Teknologi Informasi";
+            statusKuota = "Tersedia (15 Kursi)";
+            break;
+        case 2:
+            namaJurusan = "Teknik Elektro";
+            statusKuota = "Kuota Penuh!";
+            break;
+        case 3:
+            namaJurusan = "Sistem Informasi";
+            statusKuota = "Tersedia (5 Kursi)";
+            break;
+        default:
+            namaJurusan = "Tidak Diketahui";
+            statusKuota = "Error: Kode Tidak Valid";
+    }
+}
+
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+}
+
+int main() {
+    input();
+    pilihanJurusan();
+    output();
+    return 0;
+}
