dc36c21a7e9def3e1f892964a4c2113c631e529c
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-30 16:28:08 +0700

    menambahkan input kode jurusan dari user

---
+++ commit5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int kodejurusan;
+string namajurusan;
+string statusjurusan;
+
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    cin >> kodejurusan;
+}
+int main()
+{
+    return 0;
+}
