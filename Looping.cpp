0ba517e1ec63fa94669c9846a7fb5bbdf568abd0
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-03-30 17:40:08 +0700

    Add files via upload

---
+++ Looping.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int x;
+
+    // Perulangan dengan FOR
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i <= 4; i++) {
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+
+    cout << endl;
+
+    // untuk mendapatkan angka acak
+    srand(time(0));
+
+    // Perulangan dengan WHILE
+    cout << "PERULANGAN WHILE" << endl;
+    x = 1 + rand() % 10;
+
+    while (x <= 5) {
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    }
+
+    cout << "Bilangan acak while yang terakhir = " << x << endl;
+    cout << endl;
+
+    // Perulangan dengan DO WHILE
+    cout << "PERULANGAN DO...WHILE" << endl;
+    x = 1 + rand() % 10;
+
+    do {
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    } while (x <= 5);
+
+    cout << "Bilangan acak do-while yang terakhir = " << x << endl;
+    cout << endl;
+
+    return 0;
+}
